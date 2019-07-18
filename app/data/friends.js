216ac6ff6bad843f19c5f14d577286fb140d142d
conniepope <conniepope77@gmail.com>
2019-07-18 13:54:16 -0400

    home page setup

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+var friends = [
+    {
+        "name":"Connie",
+        "photo":"https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/207680_1013971310228_9748_n.jpg?_nc_cat=111&_nc_oc=AQnAgaUi6op9s2KgnpIDpS3UO7muB7LNOdfPX7ked7yuoCSYZFiRIlOU8snxmixBjPE&_nc_ht=scontent-iad3-1.xx&oh=9668b0a2424b2292c9fafd2f1b627185&oe=5DA73AA7",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }
--- server.js
+++ server.js
@@ -9,3 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+
